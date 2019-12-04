e87cec527699b176c1b5e583d54da9d61c43188d
Samuji <samuji.luck@gmail.com>
2019-12-05 06:00:25 +0700

    staging tambah form=index.php

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 $var= "programming";
 echo "Hello world, ,I'm ready to learn $var";
 
-
-
 ?>
+<html>
+<body>
+    <form action="" method="">
+        <label for="nama"> Nama : </label>    
+        <input type="text" placeholder="isi nama" id="nama">
+        <label for="job"> Job : </label>    
+        <input type="text" placeholder="isi nik anda" id="job">
+        <label for="email"> Email : </label>    
+        <input type="text" placeholder="fill your name" id="email">
+    </form>   
+</body>
+</html>
