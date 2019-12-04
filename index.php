0aaf84fae2609a19cd18b402e2e8260e9fb1cc2e
Samuji <samuji.luck@gmail.com>
2019-12-05 06:16:16 +0700

    staging tambah komponen container=index.php

--- index.php
+++ index.php
@@ -6,13 +6,17 @@ echo "Hello world, ,I'm ready to learn $var";
 ?>
 <html>
 <body>
-    <form action="" method="">
-        <label for="nama"> Nama : </label>    
-        <input type="text" placeholder="isi nama" id="nama">
-        <label for="job"> Job : </label>    
-        <input type="text" placeholder="isi nik anda" id="job">
-        <label for="email"> Email : </label>    
-        <input type="text" placeholder="fill your name" id="email">
-    </form>   
+    <container>   
+        <form action="" method="">
+            <label for="nama"> Nama : </label>    
+            <input type="text" placeholder="isi nama" id="nama">
+            </br>
+            <label for="job"> Job : </label>    
+            <input type="text" placeholder="isi nik anda" id="job">
+            </br>
+            <label for="email"> Email : </label>    
+            <input type="text" placeholder="fill your name" id="email">
+        </form>   
+    </container>
 </body>
 </html>
