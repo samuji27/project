cbe6ac938c37c85b91a0d7afaccef93dfd352778
samuji27 <57383621+samuji27@users.noreply.github.com>
2019-12-05 06:35:18 +0700

    tambah required = index.php

--- index.php
+++ index.php
@@ -9,13 +9,13 @@ echo "Hello world, ,I'm ready to learn $var";
     <container>   
         <form action="" method="">
             <label for="nama"> Nama : </label>    
-            <input type="text" placeholder="isi nama" id="nama">
+            <input type="text" placeholder="isi nama" id="nama" required>
             </br>
             <label for="job"> Job : </label>    
-            <input type="text" placeholder="isi nik anda" id="job">
+            <input type="text" placeholder="isi nik anda" id="job" required>
             </br>
             <label for="email"> Email : </label>    
-            <input type="text" placeholder="fill your name" id="email">
+            <input type="text" placeholder="fill your name" id="email" required>
         </form>   
     </container>
 </body>
